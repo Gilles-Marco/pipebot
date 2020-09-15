ec73ad109d5cd3cc370e126480801fe3263da613
Marco <gilles.marco06@gmail.com>
2020-09-15 23:52:44 +0200

    writing all needed test, need to write the code now

---
+++ test/storageManager.test.js
@@ -0,0 +1,30 @@
+import StorageManager from '../src/storage_manager.js'
+import chai from 'chai'
+
+const assert = chai.assert
+
+describe('StorageManager test', ()=>{
+    it('Get instance', ()=>{
+
+    })
+
+    it('Set info', ()=>{
+
+    })
+
+    it('Save info', ()=>{
+
+    })
+
+    it('Get info', ()=>{
+
+    })
+
+    it('Update info with server not existing', ()=>{
+
+    })
+
+    it('Update info with server existing', ()=>{
+
+    })
+})
---
+++ test/utils.test.js
@@ -0,0 +1,35 @@
+import chai from 'chai'
+
+const assert = chai.assert
+
+describe('Util test', ()=>{
+
+    it('Get channel by name with channel existing', ()=>{
+
+    })
+
+    it('Get channel by name with channel not existing', ()=>{
+
+    })
+
+    it('Get default channel from storage', ()=>{
+
+    })
+
+    it('Get default channel first channel', ()=>{
+
+    })
+
+    it('is admin true', ()=>{
+
+    })
+
+    it('is admin false', ()=>{
+
+    })
+
+    it('Get first text channel', ()=>{
+        
+    })
+
+})
