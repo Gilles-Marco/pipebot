d4d5ce91e09e7885bcec5a3b4f0521ce479fae04
Marco <gilles.marco06@gmail.com>
2020-09-15 15:25:05 +0200

    better test for give right info command

--- test/bot_command/test_command.test.js
+++ test/bot_command/test_command.test.js
@@ -2,29 +2,40 @@ import chai from 'chai'
 import dotenv from 'dotenv'
 import { get_default_channel } from '../../src/utils.js'
 import give_info from '../../src/bot_command/give_info.js'
+import version from '../../src/bot_command/version.js'
+import set_default_channel from '../../src/bot_command/set_default_channel.js'
 import Discord from 'discord.js'
+import fs from 'fs'
 
 let assert = chai.assert
 let process = dotenv.config().parsed
-let bot = new Discord.Client()
-bot.login(process.TOKEN)
 
-describe('Give info command', ()=>{
-    it('Give right info', ()=>{
-        new Promise((resolve, reject)=>{
-            let timeout = setTimeout(()=>{
-                reject('timeout')
-            }, 3000)
+function get_ready_bot(){
+    return new Promise((resolve, reject)=>{
+        let bot = new Discord.Client()
+        bot.login(process.TOKEN)
 
-            bot.on('ready', ()=>{
-                clearTimeout(timeout)
-                resolve(bot)
-            })
-        }).then((value)=>{
-            let server = value.guilds.get(value.guilds.firstKey())
-            let default_channel = get_default_channel(server)
+        let timeout = setTimeout(()=>{
+            reject('timeout')
+        }, 3000)
 
-            let right_message = `To send a message to pipe into discord's server you have to send an HTTP POST request to this URL\n\
+        bot.on('ready', ()=>{
+            clearTimeout(timeout)
+            resolve(bot)
+        })
+    })
+}
+
+describe('Bot command test', ()=>{
+    it('Give right info', async ()=>{
+        let value = await get_ready_bot().catch((error)=>{
+            assert.fail(`Error ${error}`)
+        })
+
+        let server = value.guilds.get(value.guilds.firstKey())
+        let default_channel = get_default_channel(server)
+
+        let right_message = `To send a message to pipe into discord's server you have to send an HTTP POST request to this URL\n\
     URL : ${process.URL+":"+process.PORT}\n\
     With this HTTP Request you have to send a JSON body\n\
     {\n\
@@ -33,13 +44,33 @@ describe('Give info command', ()=>{
         \t"message": <your-message-here>\n\
     }\n\
     Default channel is ${default_channel}`
-            let generated_message = give_info(server)
-            value.destroy()
-            assert(generated_message == right_message, 'Give_info doesnt give the right info')
-            // assert('a' == 'a', 'a is different from a')
-        }).catch((error)=>{
-            console.error(`Error : ${error}`)
-        })
-            
+        let generated_message = give_info(server)
+        value.destroy()
+        assert(generated_message == right_message, 'Give_info doesnt give the right info')     
+    })
+
+    it('pipebot version', ()=>{
+        const versionpkg = JSON.parse(fs.readFileSync('./package.json', {
+            encoding: 'utf-8'
+        })).version
+        let generated_message = version()
+        let right_message = `pipebot's version is : ${versionpkg}`
+        assert(generated_message == right_message, 'pipebot doesnt give the right version')
+    })
+
+    it('set_default_channel wrong_guild', () => {
+        
+    })
+
+    it('set_default_channel wrong_channel', () => {
+
+    })
+
+    it('set_default_channel not_an_admin_user', () => {
+
+    })
+
+    it('set_default_channel good', () => {
+
     })
 })
